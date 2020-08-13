f1dec7053add4f0c0cc0a5ed4d1789180ba8e15e
Wudong Liu <wudong.liu@gmail.com>
2020-08-13 11:13:58 +0100

    Add more test to improve test coverage.

--- techtest - spring experienced/src/test/java/com/db/dataplatform/techtest/api/controller/ServerControllerComponentTest.java
+++ techtest - spring experienced/src/test/java/com/db/dataplatform/techtest/api/controller/ServerControllerComponentTest.java
@@ -1,13 +1,17 @@
 package com.db.dataplatform.techtest.api.controller;
 
 import com.db.dataplatform.techtest.TestDataHelper;
+import com.db.dataplatform.techtest.common.api.model.PatchResponse;
 import com.db.dataplatform.techtest.common.api.model.PushResponse;
 import com.db.dataplatform.techtest.server.api.controller.ServerController;
 import com.db.dataplatform.techtest.common.api.model.DataEnvelope;
 import com.db.dataplatform.techtest.server.exception.HadoopClientException;
 import com.db.dataplatform.techtest.server.component.Server;
+import com.db.dataplatform.techtest.server.persistence.BlockTypeEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.nashorn.internal.ir.Block;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -20,12 +24,14 @@ import org.springframework.web.util.UriTemplate;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -72,4 +78,55 @@ public class ServerControllerComponentTest {
 		PushResponse pushResponse = objectMapper.readValue(contentAsString, PushResponse.class);
 		assertThat(pushResponse.isSuccessful()).isTrue();
 	}
+
+	@Test
+	public void testQueryDataWorksAsExpected() throws Exception {
+
+		BlockTypeEnum type = BlockTypeEnum.BLOCKTYPEA;
+
+		when(serverMock.findDataEnvelopByBlocktype(eq(type))).thenReturn(Arrays.asList(testDataEnvelope));
+		MvcResult mvcResult = mockMvc.perform(get(URI_GETDATA.expand(type))
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String contentAsString = mvcResult.getResponse().getContentAsString();
+		List list = objectMapper.readValue(contentAsString, List.class);
+		assertThat(list).hasSize(1);
+	}
+
+	@Test
+	public void testUpdatePatchCallWorksAsExpected() throws Exception {
+
+		String testDataEnvelopeJson = objectMapper.writeValueAsString(testDataEnvelope);
+
+		String name= "ABC-BDC-EDF";
+		BlockTypeEnum type = BlockTypeEnum.BLOCKTYPEA;
+		when(serverMock.updateBlocktypeByName(eq(name), eq(type))).thenReturn(true);
+		MvcResult mvcResult = mockMvc.perform(patch(URI_PATCHDATA.expand(name, type))
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		String contentAsString = mvcResult.getResponse().getContentAsString();
+		PatchResponse response = objectMapper.readValue(contentAsString, PatchResponse.class);
+		assertThat(response.isSuccessful()).isTrue();
+	}
+
+	// TODO the test instance didn''t have validation enabled, need to fix to make this test work.
+	@Ignore
+	public void testUpdatePatchInvalidNameShouldHaveClientError() throws Exception {
+
+		String testDataEnvelopeJson = objectMapper.writeValueAsString(testDataEnvelope);
+
+		String name= "ABCEDF";
+		BlockTypeEnum type = BlockTypeEnum.BLOCKTYPEA;
+		when(serverMock.updateBlocktypeByName(eq(name), eq(type))).thenReturn(true);
+		MvcResult mvcResult = mockMvc.perform(patch(URI_PATCHDATA.expand(name, type))
+				.contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().is4xxClientError())
+				.andReturn();
+	}
+
+
 }
--- techtest - spring experienced/src/test/java/com/db/dataplatform/techtest/service/ServerServiceTests.java
+++ techtest - spring experienced/src/test/java/com/db/dataplatform/techtest/service/ServerServiceTests.java
@@ -1,9 +1,11 @@
 package com.db.dataplatform.techtest.service;
 
+import com.db.dataplatform.techtest.TestDataHelper;
 import com.db.dataplatform.techtest.common.api.model.DataEnvelope;
 import com.db.dataplatform.techtest.common.util.ChecksumCalc;
 import com.db.dataplatform.techtest.server.component.HadoopClient;
 import com.db.dataplatform.techtest.server.mapper.ServerMapperConfiguration;
+import com.db.dataplatform.techtest.server.persistence.BlockTypeEnum;
 import com.db.dataplatform.techtest.server.persistence.model.DataBodyEntity;
 import com.db.dataplatform.techtest.server.persistence.model.DataHeaderEntity;
 import com.db.dataplatform.techtest.server.service.DataBodyService;
@@ -14,14 +16,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.OngoingStubbing;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.internal.util.Lists;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.db.dataplatform.techtest.TestDataHelper.createTestDataEnvelopeApiObject;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ServerServiceTests {
@@ -51,7 +57,7 @@ public class ServerServiceTests {
         expectedDataBodyEntity = modelMapper.map(testDataEnvelope.getDataBody(), DataBodyEntity.class);
         expectedDataBodyEntity.setDataHeaderEntity(modelMapper.map(testDataEnvelope.getDataHeader(), DataHeaderEntity.class));
 
-        when(checksumCalc.checksum(testDataEnvelope.getDataBody().getDataBody())).thenReturn(testDataEnvelope.getChecksum());
+        when(checksumCalc.checksum(eq(testDataEnvelope.getDataBody().getDataBody()))).thenReturn(testDataEnvelope.getChecksum());
         server = new ServerImpl(dataBodyServiceImplMock, modelMapper, checksumCalc, client);
     }
 
@@ -60,6 +66,25 @@ public class ServerServiceTests {
         boolean success = server.saveDataEnvelope(testDataEnvelope);
 
         assertThat(success).isTrue();
-        //verify(dataBodyServiceImplMock, times(1)).saveDataBody(eq(expectedDataBodyEntity));
+        verify(dataBodyServiceImplMock, times(1)).saveDataBody(eq(expectedDataBodyEntity));
+        verify(client, times(1)).pushToHadoop(eq(testDataEnvelope.getDataBody().getDataBody()));
     }
+
+    @Test
+    public void sholdUpdateDataEnvelopAsExpected() throws NoSuchAlgorithmException, IOException{
+        String name = "TSLA-GDPUSD-10Y";
+        boolean success = server.updateBlocktypeByName(name, BlockTypeEnum.BLOCKTYPEB);
+        assertThat(success).isTrue();
+        verify(dataBodyServiceImplMock, times(1)).updateBlocktypeByName(eq(name), eq(BlockTypeEnum.BLOCKTYPEB));
+    }
+
+    @Test
+    public void shouldQueryDataAsExpected() throws NoSuchAlgorithmException, IOException{
+        when(dataBodyServiceImplMock.getDataByBlockType(eq(BlockTypeEnum.BLOCKTYPEB))).thenReturn(Arrays.asList(expectedDataBodyEntity));
+        List<DataEnvelope> dataEnvelopByBlocktype = server.findDataEnvelopByBlocktype(BlockTypeEnum.BLOCKTYPEB);
+        assertThat(dataEnvelopByBlocktype).hasSize(1);
+        assertThat(dataEnvelopByBlocktype.get(0).getDataBody().getDataBody()).isEqualTo(TestDataHelper.DUMMY_DATA);
+        verify(dataBodyServiceImplMock, times(1)).getDataByBlockType(eq(BlockTypeEnum.BLOCKTYPEB));
+    }
+
 }
