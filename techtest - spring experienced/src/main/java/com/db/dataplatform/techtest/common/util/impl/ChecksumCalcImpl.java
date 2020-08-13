e559220d43e307d7d6cef777916057cc788ccdda
Wudong Liu <wudong.liu@gmail.com>
2020-08-13 09:55:52 +0100

    Change encoding for md5 checksum just to use hex.

--- techtest - spring experienced/src/main/java/com/db/dataplatform/techtest/common/util/impl/ChecksumCalcImpl.java
+++ techtest - spring experienced/src/main/java/com/db/dataplatform/techtest/common/util/impl/ChecksumCalcImpl.java
@@ -3,6 +3,7 @@ package com.db.dataplatform.techtest.common.util.impl;
 import com.db.dataplatform.techtest.common.util.ChecksumCalc;
 import org.springframework.stereotype.Component;
 
+import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -12,19 +13,17 @@ import java.util.Base64;
 public class ChecksumCalcImpl implements ChecksumCalc {
 
     private final MessageDigest mdDigester;
-    private final Base64.Encoder encoder;
 
 
     public ChecksumCalcImpl() throws NoSuchAlgorithmException {
         this.mdDigester = MessageDigest.getInstance("MD5");
-        this.encoder = Base64.getEncoder();
+
     }
 
     @Override
     public String checksum(String content) {
         mdDigester.update(content.getBytes());
         byte[] digest = mdDigester.digest();
-        byte[] encode = this.encoder.encode(digest);
-        return new String(encode, StandardCharsets.ISO_8859_1);
+        return DatatypeConverter.printHexBinary(digest);
     }
 }
